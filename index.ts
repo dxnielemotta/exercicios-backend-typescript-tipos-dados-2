687c811aa912c3e898c166c9ac79946fa597ce76
dxnielemotta <danielemottarocha@gmail.com>
2024-07-20 08:41:24 -0300

    resolução ex 1

---
+++ index.ts
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const lerArquivo = (): unknown => {
+  return JSON.parse(fs.readFileSync("./bd.json"));
+};
+
+const escreverArquivo = (dados: any): void => {
+  fs.writeFileSync("./bd.json", JSON.stringify(dados));
+};
+
+const dados = lerArquivo() as string[];
+dados.push("Amanda");
+
+escreverArquivo(dados);
+
+console.log(lerArquivo());
