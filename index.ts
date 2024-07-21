e92ffd9f3ffa4ea98b304037b42a21979f0dccf5
dxnielemotta <danielemottarocha@gmail.com>
2024-07-21 12:16:34 -0300

    resolução

--- index.ts
+++ index.ts
@@ -8,9 +8,87 @@ const escreverArquivo = (dados: any): void => {
   fs.writeFileSync("./bd.json", JSON.stringify(dados));
 };
 
-const dados = lerArquivo() as string[];
-dados.push("Amanda");
+type Endereco = {
+  cep: string;
+  rua: string;
+  compelemento?: string;
+  bairro: string;
+  cidade: string;
+};
 
-escreverArquivo(dados);
+type Usuario = {
+  nome: string;
+  email: string;
+  cpf: string;
+  profissao?: string;
+  endereco: Endereco | null;
+};
 
-console.log(lerArquivo());
+const cadastrarUsuario = (dados: Usuario): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+
+  bd.push(dados);
+
+  escreverArquivo(bd);
+  return dados;
+};
+
+const listarUsuarios = (filtro?: string): Usuario[] => {
+  const bd = lerArquivo() as Usuario[];
+
+  const usuarios = bd.filter((usuario) => {
+    if (filtro) {
+      return usuario.profissao === filtro;
+    }
+    return usuario;
+  });
+
+  return usuarios;
+};
+
+const detalharUsuario = (cpf: string): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("Usuário não encontrado");
+  }
+  return usuario;
+};
+
+const atualizarUsuario = (cpf: string, dados: Usuario) => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("Usuário não encontrado");
+  }
+
+  Object.assign(usuario, dados);
+
+  escreverArquivo(bd);
+
+  return dados;
+};
+
+const excluirUsuario = (cpf: string): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("Usuário não encontrado");
+  }
+
+  const exclusao = bd.filter((usuario) => {
+    return usuario.cpf !== cpf;
+  });
+
+  escreverArquivo(exclusao);
+  return usuario;
+};
